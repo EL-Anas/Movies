49f1b12c97ac5b3b9c108f49fe5ee14015a137eb
elalaoui anas <anas.elalaoui2000@gmail.com>
2022-11-25 16:29:25 +0100

    add bg to detail page

--- src/app/Model/Film.ts
+++ src/app/Model/Film.ts
@@ -10,8 +10,9 @@ export class Film{
     popularity:string;
     vote_count:string;
     runtime:string;
+    backdrop_path:string;
 
-    constructor(id:number,titre:string,original_title:string,imageUrl:string,overview:string,vote_average:string,release_date:string,original_language:string,popularity:string,vote_count:string,runtime:string){
+    constructor(id:number,titre:string,original_title:string,imageUrl:string,overview:string,vote_average:string,release_date:string,original_language:string,popularity:string,vote_count:string,runtime:string,backdrop_path:string){
         this.id=id;
         this.title=titre;
         this.original_title=original_title;
@@ -22,7 +23,8 @@ export class Film{
         this.original_language=original_language;
         this.popularity=popularity;
         this.vote_count=vote_count;
-        this.runtime=runtime
+        this.runtime=runtime;
+        this.backdrop_path=backdrop_path
 
     }
 
